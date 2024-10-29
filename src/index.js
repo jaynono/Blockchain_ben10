e49a2bd618d741ed2ff59c94464101d014e3deb5
Aphrodite <162393251+Zeus-Aphrodite@users.noreply.github.com>
2024-10-29 10:37:00 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ root.render(
   <App />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+//is blockchain ben10 project.
+// to report always console result
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
